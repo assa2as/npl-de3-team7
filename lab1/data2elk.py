b199471b0621a38384fd4cf42cf75f30c3694c35
Ubuntu <ubuntu@instance-1.europe-west1-b.c.de3-npl.internal>
2018-10-16 10:21:51 +0000

    lab1 commit

---
+++ lab1/data2elk.py
@@ -0,0 +1,25 @@
+import json
+import codecs
+from elasticsearch import Elasticsearch
+from elasticsearch import helpers
+
+es = Elasticsearch()
+
+actions = []
+with codecs.open('data/item_details_full.txt',encoding='utf-8') as f:
+  for i,line in enumerate(f):
+    rec = json.loads(line)
+    action = {
+    "_index": "items-index",
+    "_type": "items",
+    "_id": int(rec["itemid"]),
+    "_source": {"name":rec.get("attr1",""),"annotation":rec.get("attr0","")}
+    }
+    actions.append(action)
+    if (i+1) % 100000 == 0:
+      print i
+      helpers.bulk(es,actions)
+      actions = []
+if len(actions) > 0:
+  helpers.bulk(es,actions)
+print i
